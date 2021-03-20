320232a402220f6cd606091f41f73d1dfa189caa
Pradeep Kumar Manimuthu <prade.ycsm@gmail.com>
2021-03-20 09:03:14 +0530

    find longest sub string updated doc string

--- longest_substring.py
+++ longest_substring.py
@@ -2,7 +2,7 @@
 Approach 1: iterate through the loop once
 create a dict to hold unique substring with key as length of the substring
 in the iteration initialise a list, add each char if not already available in list
-    when a char is already in the list, add the contents to the set and clear list and add current item to the list
+    when a char is already in the list, add the contents to the dict and slice the list and get char after the duplicate and add current item to the list
 """
 
 def longest(s):
@@ -10,7 +10,6 @@ def longest(s):
     l = []
     for char in s:
         if char in l:
-            # ss = ''.join(l[:l.index(char)+1])
             ss = ''.join(l)
             u[len(ss)] = (ss)
             l = l[l.index(char)+1:]
